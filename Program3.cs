6209f9fdad3ea4cac732d530e9b0bf483aeec876
byblik-0 <azbx1840az09b.x@gmail.com>
2025-11-10 23:04:32 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ObjectCounter obj = new ObjectCounter();
+        ObjectCounter obj1 = new ObjectCounter();
+        ObjectCounter.ShowCount();
+        ObjectCounter obj2 = new ObjectCounter();
+        ObjectCounter.ShowCount();
+    }
+
+}
+internal class ObjectCounter
+{
+    private static int count = 0;
+
+    public ObjectCounter()
+    {
+        count++;
+    }
+    public static void ShowCount()
+    {
+        Console.WriteLine("Количество экземпляров(обьектов): " + count);
+    }
+
+}
