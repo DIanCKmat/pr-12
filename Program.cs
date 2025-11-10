e847b72267e6be249362b6fdf7e95b6ba3f91b78
byblik-0 <azbx1840az09b.x@gmail.com>
2025-11-10 23:09:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Settings.ShowConfigFilePath();
+    }
+
+}
+internal class Settings
+{
+    private static readonly string ConfigFilePath;
+
+    static Settings()
+    {
+        ConfigFilePath = @"C:\config\settings.json";
+    }
+    public static void ShowConfigFilePath()
+    {
+        Console.WriteLine(ConfigFilePath);
+    }
+}
