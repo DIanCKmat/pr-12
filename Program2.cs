9923e080213cf7a124af03f7ffe070c407d20fd6
byblik-0 <azbx1840az09b.x@gmail.com>
2025-11-10 23:02:32 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProgramConfig.Info();
+    }
+
+}
+internal class ProgramConfig
+{
+    public const string Version = "1.0.0";
+    public const string Name = "Пофистал Бабаев";
+
+    public static void Info()
+    {
+        Console.WriteLine($"Версия программы: {Version}");
+        Console.WriteLine($"Имя разработчика: {Name}");
+    }
+}
