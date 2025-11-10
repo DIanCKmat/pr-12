9187892ca73f4dabd5722eb200bac27f38950fb2
byblik-0 <azbx1840az09b.x@gmail.com>
2025-11-10 23:06:36 +0300

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Book book = new Book("678-673-636-366", "Роковые яйца", "Михаил Булгаков");
+        book.ShowInfoBook();
+    }
+
+}
+internal class Book
+{
+    private readonly string _isbn;
+    public string ISBN { get { return _isbn; } }
+    private string _title;
+    public string Title { get { return _title; } set { _title = value; } }
+
+    private string _author;
+    public string Author { get { return _author; } set { _author = value; } }
+
+    public Book(string isbn, string title, string author)
+    {
+        _isbn = isbn;
+        _title = title;
+        _author = author;
+    }
+
+    public void ShowInfoBook()
+    {
+        Console.WriteLine($"ISBN: {ISBN}");
+        Console.WriteLine($"Название: {Title}");
+        Console.WriteLine($"Автор: {Author}");
+    }
+
+
+}
